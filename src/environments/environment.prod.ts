28f16b6c268da1fce413fe26a544c8a7c0821449
khalisabrahman <khalisabrahman@gmail.com>
2023-06-01 12:00:44 +0800

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://youtube-v31.p.rapidapi.com',
+  API_URL: 'https://youtube-v31.p.rapidapi.com',
   API_KEY: '228906f4a3msh04b732610d8239ep15f266jsn964f3d542b77',
   AI_HOST: 'youtube-v31.p.rapidapi.com'
 };
